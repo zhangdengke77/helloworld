b42589707fa504767b66726abbcfc8636a859721
zhangdengke77 <56582003+zhangdengke77@users.noreply.github.com>
2019-10-17 16:11:48 +0800

    第二次修改，把爬虫内容传上来了

--- first import.py
+++ first import.py
@@ -1,1 +1,11 @@
 print('helloworld')
+from bs4 import BeautifulSoup
+import requests
+
+baidu = requests.get('http://www.baidu.com').content
+soul = BeautifulSoup(baidu,'html.parser')
+links = soul.findAll('a')
+for link in links:
+
+
+    print(link.string)
